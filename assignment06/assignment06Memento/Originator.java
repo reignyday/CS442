07019e702ddf6425173dfc1dc9984967b88b6ca6
reignyday <33501252+reignyday@users.noreply.github.com>
2025-05-10 14:02:37 -0400

    Add files via upload
    updated final, working

--- assignment06/assignment06Memento/Originator.java
+++ assignment06/assignment06Memento/Originator.java
@@ -9,9 +9,11 @@ public class Originator {
 	
 	public void reset (Board board, Memento mem) {
 		MoveMemento mmem = (MoveMemento)mem;
-		board.setPiece(GET THE FROM LOCATION* from mmem, GET THE FROM PIECE from mmem);
-		board.setPiece(GET THE TO LOCATION* from mmem, GET THE TO PIECE from mmem);
-		*NOTE the LOCATION will be 2 digits using the ICCF notation--the LOCATIONS
-		are split from the iccf in mmem using from = iccf/100 and to = iccf%100
+		int from = mmem.getIccf()/100;
+		int to = mmem.getIccf() % 100;
+		board.setPiece(from, mmem.getFromPiece());
+		board.setPiece(to, mmem.getToPiece());
+		//*NOTE the LOCATION will be 2 digits using the ICCF notation--the LOCATIONS
+		//are split from the iccf in mmem using from = iccf/100 and to = iccf%100
 	}
 }
